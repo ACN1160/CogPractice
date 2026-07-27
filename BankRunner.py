806dc826fa85ec398b7877ff770736d799e8bd86
ACN1160 <acnolan@gmail.com>
2026-07-27 11:44:09 -0400

    Added BankRunner.py file to the project. This file contains the main logic for running the bank application, including user interactions and transaction processing.

---
+++ BankRunner.py
@@ -0,0 +1,43 @@
+class user():
+    name = "Empty"
+    password = "Empty"
+
+    def __init__(self, name, password):
+        self.password = password
+        self.name = name
+
+class admin(user):
+    def __init__(self, name, password):
+        super().__init__(name, password)
+
+
+class bankApp():
+    DB = {}
+    DB["admin"] = "admin123"
+    DB["Alice"] = "AE1234"
+    DB["Bob"] = "BE1234"
+    DB["Charles"] = "CE1234"
+    DB["Dawde"] = "DE1234"
+
+    def printMessage(output):
+        print(output)
+    def myLogin():
+        print("Enter username and password seperated by a space")
+        name,password = input().split(" ")
+        if name in bankApp.DB and bankApp.DB[name] == password:
+            return(name,password)
+        else:
+            return(None,None)
+
+
+    def main():    
+        bankApp.printMessage("Welcome to this app")
+        name, password = bankApp.myLogin()
+        if name is not None:
+            bankApp.printMessage("Login successful")
+        else:
+            bankApp.printMessage("Login failed")
+
+
+if __name__ =="__main__":
+    bankApp.main()
