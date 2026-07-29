fc1bd5255677ef22bcf39b02f72e2d20fda9f9aa
ACN1160 <acnolan@gmail.com>
2026-07-28 22:15:04 -0400

    Updated customer repo file

--- RestAPIwoDB/app/repo/repo_customers.py
+++ RestAPIwoDB/app/repo/repo_customers.py
@@ -5,4 +5,9 @@ class CustomerRepo():
     def get_customers(self):
         return customers.objects()
     def get_customer(self, cus_id):
-        return customers.objects(id = cus_id).first()
+        return customers.objects(id=cus_id).first()
+
+    def add_customer(self, customer_data):
+        customer = customers(**customer_data)
+        customer.save()
+        return customer
