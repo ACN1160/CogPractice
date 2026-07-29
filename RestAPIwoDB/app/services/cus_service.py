034ee81d828390aa79b1732a79c08e70f746d935
ACN1160 <acnolan@gmail.com>
2026-07-29 15:06:40 -0400

    Added changesto json

--- RestAPIwoDB/app/services/cus_service.py
+++ RestAPIwoDB/app/services/cus_service.py
@@ -8,13 +8,20 @@ class CustomerService:
     def get_customer(self, customer_id):
         return self._repo.get_customer(customer_id)
 
-    def add_customer(self, first_name, last_name, customer_email):
+    def add_customer(self, data):
+        first_name = data.get('firstname')
+        last_name = data.get('lastname')
+        email = data.get('email')
+
         customer_data = {
             "first_name": first_name,
             "last_name": last_name,
-            "email": customer_email,
+            "email": email,
         }
         return self._repo.add_customer(customer_data)
 
-    def update_customer(self, first_name, last_name, customer_email):
-        return self._repo.update_customer(first_name, last_name, customer_email)
+    def update_customer(self, id, data):
+        first_name = data.get('firstname') if 'firstname' in data else None
+        last_name = data.get('lastname') if 'lastname' in data else None
+        email = data.get('email') if 'email' in data else None
+        return self._repo.update_customer(id, first_name, last_name, email)
