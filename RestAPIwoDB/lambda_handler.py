4e448d5f3e04de7157bde10cf151d79251b9d189
ACN1160 <acnolan@gmail.com>
2026-07-31 20:43:10 -0400

    Change serverlesswsgi.response to handle_request

--- RestAPIwoDB/lambda_handler.py
+++ RestAPIwoDB/lambda_handler.py
@@ -4,4 +4,4 @@ from app import create_app
 app = create_app()
 
 def handler(event, context):
-    return serverless_wsgi.response(app, event, context)
+    return serverless_wsgi.handle_request(app, event, context)
